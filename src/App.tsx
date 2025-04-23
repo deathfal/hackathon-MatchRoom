bb209604e7fcd9aa05f1f4cb3ae6b3c1b3b8ea75
Mathieu Keca <mathieu.keca@gmail.com>
2025-04-23 14:50:21 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import { Routes, Route } from 'react-router-dom'
-import HotelQuestionnaire from './components/HotelQuestionnaire'
-import Home from './Pages/Home';
-import Favorites from './Pages/favorites';
-import Login from './Pages/Login';
-import Preferences from './Pages/Preferences';
-import Profil from './Pages/Profil';
-import Rewards from './Pages/Rewards';
-import SignupCustomer from './Pages/SignupCustomer';
-import SignupHotel from './Pages/SignupHotel';
+import Questionnaire from './Pages/Questionnaire'
+// import Home from './Pages/Home';
+// import Favorites from './Pages/favorites';
+// import Login from './Pages/Login';
+// import Preferences from './Pages/Preferences';
+// import Profil from './Pages/Profil';
+// import Rewards from './Pages/Rewards';
+// import SignupCustomer from './Pages/SignupCustomer';
+// import SignupHotel from './Pages/SignupHotel';
 
 function App() {
   return (
     <div className="w-full h-full">
       <Routes>
-        <Route path="/hotel-questionnaire" element={<HotelQuestionnaire />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/questionnaire" element={<Questionnaire />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/login" element={<Login />} />
         <Route path="/preferences" element={<Preferences />} />
         <Route path="/profil" element={<Profil />} />
         <Route path="/rewards" element={<Rewards />} />
         <Route path="/signup-customer" element={<SignupCustomer />} />
-        <Route path="/signup-hotel" element={<SignupHotel />} />
+        <Route path="/signup-hotel" element={<SignupHotel />} /> */}
       </Routes>
     </div>
   )
