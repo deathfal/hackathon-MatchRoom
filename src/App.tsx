1af6dd410909d6e60e671bbe603dba2933214add
Remy Saguez <resaguez.2800@gmail.com>
2025-04-23 14:44:11 +0200

    pages route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import HotelQuestionnaire from './components/HotelQuestionnaire'
 import Home from './Pages/Home';
+import Favorites from './Pages/favorites';
+import Login from './Pages/Login';
+import Preferences from './Pages/Preferences';
+import Profil from './Pages/Profil';
+import Rewards from './Pages/Rewards';
+import SignupCustomer from './Pages/SignupCustomer';
+import SignupHotel from './Pages/SignupHotel';
 
 function App() {
   return (
@@ -8,6 +15,13 @@ function App() {
       <Routes>
         <Route path="/hotel-questionnaire" element={<HotelQuestionnaire />} />
         <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/preferences" element={<Preferences />} />
+        <Route path="/profil" element={<Profil />} />
+        <Route path="/rewards" element={<Rewards />} />
+        <Route path="/signup-customer" element={<SignupCustomer />} />
+        <Route path="/signup-hotel" element={<SignupHotel />} />
       </Routes>
     </div>
   )
