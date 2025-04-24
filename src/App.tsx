d3f9f2b66248a1d400c90f01355f9264f8a7de07
Remy Saguez <resaguez.2800@gmail.com>
2025-04-24 18:14:20 +0200

    nego

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Search  from './Pages/Search';
 import DashboardHotel from './Pages/DashboardHotel';
 import DashboardAdmin from './Pages/DashboardAdmin';
 import Login from './Pages/Login';
+import Negociation from './Pages/Negociation';
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
         <Route path="/signup-type" element={<SignupType />} />
         <Route path="/signup-complete" element={<SignupComplete />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/negociation" element={<Negociation />} />
       </Routes>
     </div>
   )
